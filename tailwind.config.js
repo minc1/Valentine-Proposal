74b23ce467c014fe0806b66c62a32e74e3c02768
Min Cho <42979910+minc1@users.noreply.github.com>
2025-01-21 20:55:57 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add any other paths where Tailwind classes might be used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
